49e41a8f02f7d5d6f547cb78c4b83005d720fa3b
Karol <k.janicki13@gmail.com>
2018-04-05 14:40:15 +0200

    Initial commit

---
+++ src/networking/Udp/UdpServer.java
@@ -0,0 +1,48 @@
+package networking.Udp;
+ 
+import java.io.File;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.nio.file.Files;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import projekt.Packet;
+import projekt.Tools;
+
+public class UdpServer {
+    public static void main(String[] args) {
+    	DatagramSocket aSocket = null;
+      try {
+        // args contain message content and server hostname
+        aSocket = new DatagramSocket(9876);
+        byte[] buffer = new byte[1024];
+        while(true) {
+          DatagramPacket request = new DatagramPacket(buffer, buffer.length);
+          System.out.println("Waiting for request...");
+          aSocket.receive(request);
+          Packet p = (Packet) Tools.deserialize(request.getData());
+          File name = new File(p.getDevice()+p.getDescription());
+          Files.write(name.toPath(), request.getData());
+          String t = new String(request.getData());
+          System.out.println("Received: " + t);
+          DatagramPacket reply = new DatagramPacket(request.getData(), request.getLength(), 
+          		request.getAddress(), request.getPort());
+          aSocket.send(reply); 
+        }
+      } catch (SocketException ex) {
+        Logger.getLogger(UdpServer.class.getName()).log(Level.SEVERE, null, ex);
+      } catch (IOException ex) {
+        Logger.getLogger(UdpServer.class.getName()).log(Level.SEVERE, null, ex);
+      } catch (ClassNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} finally {
+				aSocket.close();
+			}
+      
+    }
+}
+
